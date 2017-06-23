05a1ecdae178bb0aa4b341dc8302191bcacee5cc
dparso <dylan.parsons1@gmail.com>
2017-06-23 11:00:01 -0400

    update

--- html/makedata.cpp
+++ html/makedata.cpp
@@ -14,25 +14,24 @@ int main() {
 
 	clock_t start = clock();
 
+	// sit in transmitting loop
 	while(true) {
 		clock_t diff = clock() - start;
 		double ratio = (double) diff / CLOCKS_PER_SEC;
 		// every second, write data and call PHP script
 		if(ratio >= 1) {
-			cout << CLOCKS_PER_SEC << endl;
-			cout << "msec is " << ratio << endl;
 			start = clock();
+			// each time open is called, file contents are cleared
 			myfile.open("data.txt");
 			for(int i = 0; i < 10; i++) {
 				double randNum = ((double) rand()) / INT_MAX; 
 				myfile << randNum << "\n";
 			}	
-			myfile.close();
 
+			myfile.close();
 			system("php sqlconnect.php");
 		}
 	}	
 
 	return 0;	
-
 }
--- html/sqlconnect.php
+++ html/sqlconnect.php
@@ -3,28 +3,30 @@
 // Read data from file
 $lines = file('data.txt');
 
-foreach ($lines as $line_num => $line) {
+/*foreach ($lines as $line_num => $line) {
 	echo "Line #{$line_num}: " . htmlspecialchars($line) . "\n";
 }
-
+*/
 // Store as integers
 $integers = array();
 for($i = 0; $i < 10; $i++) {
 	$integers[$i] = floatval($lines[$i]);
 	echo $integers[$i] . "\n";
 }
+echo "(" . count($integers) . " elements)\n";
 
 $servername = "turing.bowdoin.edu";
+//$servername = "localhost";
 $username = "energydata";
 $password = "zxZbLKC5fdvNPOpw";
 $dbname = "energydata";
 
 // Create connection
-//$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-/*if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}*/
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error . "\n");
+}
 
 // Insert each value individually
 $sql = "";
@@ -36,11 +38,12 @@ foreach ($integers as $integer) {
 
 echo "sql is \n${sql}";
 
-/*if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE) {
 	echo "New record created successfully";
 } else {
 	echo "Error: " . $sql . "<br>" . $conn->error;
-}*/
+}
 
-//$conn->close();
+$conn->close();
+
 ?>
